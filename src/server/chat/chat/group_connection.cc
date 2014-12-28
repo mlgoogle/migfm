c10c2f675467a1c8db18b10aebebbe712b806758
kerry <kerry@miglab.com>
2014-12-28 13:10:39 +0800

    +群组聊天管理 *修改MAKE文件

---
+++ src/server/chat/chat/group_connection.cc
@@ -0,0 +1,10 @@
+/*
+ * group_connection.cc
+ *
+ *  Created on: 2014年12月16日
+ *      Author: kerry
+ */
+
+
+
+
---
+++ src/server/chat/chat/group_connection.h
@@ -0,0 +1,15 @@
+/*
+ * group_connection.h
+ *
+ *  Created on: 2014年12月16日
+ *      Author: kerry
+ */
+
+#ifndef GROUP_CONNECTION_H_
+#define GROUP_CONNECTION_H_
+
+
+
+
+
+#endif /* GROUP_CONNECTION_H_ */
